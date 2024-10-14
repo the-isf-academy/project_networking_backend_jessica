cbe44c3217ee06d5ea3cf8bec8fb03e5bc746e53
Jessica <0014068@student.isf.edu.hk>
2024-10-14 11:54:23 +0800

    #today wrote model and tested #next work  on the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class Palette(Model):
+    hex1 = IntegerField()
+    hex2 = IntegerField()
+    hex3 = IntegerField()
+    hex4 = IntegerField()
+#    tone = StringField()
+#    likes = IntegerField()
+#    archive = BooleanField()
+
+    def json_response(self):
+        
+        return {
+            'id': self.id,
+            'hex1': self.hex1,
+            'hex2': self.hex2,
+            'hex3': self.hex3,
+            'hex4': self.hex4,
+            #'likes': self.likes,
+            #'tone': self.tone
+        }
--- app/views.py
+++ app/views.py
@@ -2,4 +2,29 @@
 
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
+from .models import Palette
 
+@route_get(BASE_URL + 'all')
+def all_palette(args):
+    palette_list = []
+
+    for palette in Palette.objects.all():
+        palette_list.append(palette.json_response())
+
+    return {'palette':palette_list}
+
+@route_post(BASE_URL + 'new', args={'hex1':int,'hex2':int,'hex3':int,'hex4':int})
+def new_palette(args):
+    new_palette = Palette(
+        hex1 = args['hex1'],
+        hex2 = args['hex2'],
+        hex3 = args['hex3'],
+        hex4 = args['hex4'],
+        #is_happy = args['is_happy'],
+        #likes = 0,
+        #archive = False
+    )
+
+    new_fortune.save()
+
+    return {'fortune': new_fortune.json_response()}
